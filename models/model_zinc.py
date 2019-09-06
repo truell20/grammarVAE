1747672b35326034d6919d52490b8739306b7465
Jonathan Yin <jonathanyin.12@gmail.com>
2019-09-06 18:13:20 -0400

    remove .core

--- models/model_zinc.py
+++ models/model_zinc.py
@@ -3,8 +3,7 @@ import tensorflow as tf
 from tensorflow.keras import backend as K
 from tensorflow.keras.losses import BinaryCrossentropy
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense, Lambda
-from tensorflow.keras.layers.core import Dense, Activation, Flatten, RepeatVector
+from tensorflow.keras.layers import Input, Dense, Lambda, Activation, Flatten, RepeatVector
 from tensorflow.keras.layers.wrappers import TimeDistributed
 from tensorflow.keras.layers.recurrent import GRU
 from tensorflow.keras.layers.convolutional import Convolution1D
