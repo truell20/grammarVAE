af2b1b719ad423d2abddbf5289db4e63d3c15466
Michael Truell <michael_truell@horacemann.org>
2019-10-19 03:38:16 -0400

    another tweak

--- models/model_zinc.py
+++ models/model_zinc.py
@@ -141,7 +141,7 @@ class MoleculeVAE():
                 x_decoded_mean = K.flatten(x_decoded_mean)
                 xent_loss = max_length * binary_crossentropy(x, x_decoded_mean)
             elif K.int_shape(pred_decoded_mean)[1] == max_length_func:
-                f_decoded_mean = conditional(true[1], pred_functional, max_length_func, 1) # we add this new function to the loss
+                f_decoded_mean = conditional(true[1], pred_decoded_mean, max_length_func, 1) # we add this new function to the loss
                 f = K.flatten(true[1])
                 f_decoded_mean = K.flatten(f_decoded_mean)
                 xent_loss = max_length_func * binary_crossentropy(f, f_decoded_mean)
