055f9eef87eccb20cfaba3abf0ca116bcae19ebc
Michael Truell <michael_truell@horacemann.org>
2019-10-19 11:49:41 -0400

    another tweak

--- models/model_zinc.py
+++ models/model_zinc.py
@@ -150,10 +150,10 @@ class MoleculeVAE():
                 x_decoded_mean = K.flatten(x_decoded_mean)
                 xent_loss = max_length * binary_crossentropy(x, x_decoded_mean)
             elif K.int_shape(pred_decoded_mean)[1] == max_length_func:
-                f_decoded_mean = conditional(true, pred_decoded_mean, max_length_func, 1) # we add this new function to the loss
-                f = K.flatten(true)
-                f_decoded_mean = K.flatten(f_decoded_mean)
-                xent_loss = max_length_func * binary_crossentropy(f, f_decoded_mean)
+                #f_decoded_mean = conditional(true, pred_decoded_mean, max_length_func, 1) # we add this new function to the loss
+                #f = K.flatten(true)
+                #f_decoded_mean = K.flatten(f_decoded_mean)
+                xent_loss = max_length_func * crossentropy(f, f_decoded_mean)
             else:
                 raise ValueError('UNRECOGNIZED SHAPE')
 
