4195b65a57dbffbe2f6d399177790244c23274e1
Jonathan Yin <jonathanyin.12@gmail.com>
2020-07-19 12:46:56 -0400

    Update model_zinc.py

--- models/model_zinc.py
+++ models/model_zinc.py
@@ -177,7 +177,7 @@ class MoleculeVAE():
         hf = Reshape((-1, latent_rep_size), name='prediction_reshape')(l)
         hf = Dense(512, name='dense_tower_1', activation = 'relu')(hf)	
         hf = Flatten(name='tower_2_flatten_1')(hf)
-        hf = Dense(200, name='dense_tower_2', activation = 'sigmoid')(l)
+        hf = Dense(200, name='dense_tower_2', activation = 'sigmoid')(hf)
         hf = Reshape((200, 1), name='decoded_mean_2')(hf)
 
         # Tower 1
